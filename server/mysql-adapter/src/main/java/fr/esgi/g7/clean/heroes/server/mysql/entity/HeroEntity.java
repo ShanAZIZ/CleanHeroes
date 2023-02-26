fcfe9bdd1446993a367c85f8ec6314944c4aff9d
saziz <shan.azizh@gmail.com>
2023-02-26 17:40:31 +0100

    feat: hero and player entity

---
+++ server/mysql-adapter/src/main/java/fr/esgi/g7/clean/heroes/server/mysql/entity/HeroEntity.java
@@ -0,0 +1,40 @@
+package fr.esgi.g7.clean.heroes.server.mysql.entity;
+
+import fr.esgi.g7.clean.heroes.domain.functional.model.Scarcity;
+import fr.esgi.g7.clean.heroes.domain.functional.model.Speciality;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "hero")
+public class HeroEntity {
+    @Id @EqualsAndHashCode.Include
+    UUID id;
+    @Column(unique = true)
+    String name;
+    @Column
+    int health;
+    @Column
+    int experience = 0;
+    @Column
+    int power;
+    @Column
+    int armor;
+    @Column // TODO: manage enum in persisting
+    Speciality speciality;
+    @Column
+    Scarcity scarcity;
+    @Column
+    int level = 1;
+}
